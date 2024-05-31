bcdf1fc8e614bf9633840b0ed6bf8ed42babf710
chandu <chandu77chandu77@gmail.com>
2024-05-31 14:07:54 +0530

    kjdhg

---
+++ day-12-foreach/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "sandbox" {
+ ami = var.ami
+ instance_type = var.instance_type
+ for_each = toset(var.sandboxes)
+ # count = length(var.sandboxes)
+
+ tags = {
+   name = each.value #for a set, each.value and each.key is the same
+ }
+
+  
+}
